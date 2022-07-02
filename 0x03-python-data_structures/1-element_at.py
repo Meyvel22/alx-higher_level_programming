91ebf630b4ca626053682f189395275955a42899
Mavel Portia Antwi <meyvel22@gmail.com>
2022-07-02 01:16:23 -0700

    Task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    #element_at - function that retrieves an element from a list like in c
+
+    if idx < 0 or idx >= len(my_list):
+        return (None)
+    return (my_list[idx])
