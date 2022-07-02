922bba769a2cab2ac03dc95ffd0c052f0eb472b3
Mavel Portia Antwi <meyvel22@gmail.com>
2022-07-02 01:15:22 -0700

    Added task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     # print_list_integer - function that prints all integers of a list
 
     for x in range(len(my_list)):
-            print("{:d}".format(my_list[x]))
+        print("{:d}".format(my_list[x]))
