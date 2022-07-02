29c041ae9b4c7fad4821b1fef214b4e88f07ea2a
Mavel Portia Antwi <meyvel22@gmail.com>
2022-07-02 01:35:23 -0700

    Added task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    #print_reversed_list_integer - prints all integers of a list in reverse
+    # print_reversed_list_integer - prints all integers of a list in reverse
 
     if my_list:
         for x in reversed(my_list):
