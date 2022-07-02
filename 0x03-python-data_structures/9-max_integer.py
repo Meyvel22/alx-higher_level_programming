5b50c1db0eeeaafa69447eff786323ce89bbed87
Mavel Portia Antwi <meyvel22@gmail.com>
2022-07-02 04:02:01 -0700

    Added task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,10 +4,10 @@ def max_integer(my_list=[]):
 
     if len(my_list) == 0:
         return (None)
+
     max_num = my_list[0]
     for x in range(len(my_list)):
         if max_num < my_list[x]:
             max_num = my_list[x]
-
 
     return (max_num)
