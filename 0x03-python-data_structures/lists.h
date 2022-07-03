8bef7a2d1ec41084e889fdb5ff1d52898c18a606
Mavel Portia Antwi <meyvel22@gmail.com>
2022-07-03 06:45:36 -0700

    Done task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -23,6 +23,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-size_t n_listint(const listint_t *h);
 
 #endif /* LISTS_H */
