3890dd84d686992c4ea29dd458bbb0e171b97924
Mavel Portia Antwi <meyvel22@gmail.com>
2022-07-06 09:54:55 -0700

    Advanced 02

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    # square_matrix_map - computes the square value of all integers of a matrix
-
     return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
