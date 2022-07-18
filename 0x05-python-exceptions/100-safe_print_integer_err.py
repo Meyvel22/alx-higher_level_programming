2eb8cb53d188352bbed831ee39b9e428a3b5dcfb
Mavel Portia Antwl <meyvel@gmail.com>
2022-07-18 09:15:59 -0700

    Advanced 0

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
     # safe_print_integer_err - prints an integer
+
+    import sys
     try:
         print("{:d}".format(value))
         return True
