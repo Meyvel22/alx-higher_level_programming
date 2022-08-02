3120f9938edb55ac92c73c889c0d412daa2e9922
Mavel Portia Antwi <meyvel22@gmail.com>
2022-08-02 09:25:00 -0700

    Task 10-12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    12-pascal_triangle: pascal_triangle()
+"""
+
+
+def pascal_triangle(n):
+    """
+        returns a lis of lists of integers
+        Args:
+            n (int): number of lists and digits
+        Returns: list of lists
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
