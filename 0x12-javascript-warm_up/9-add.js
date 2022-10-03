33dbb1ea113ae20d4351ac6c27d0ec416269f217
Meyvel22 <meyvel22@gmail.com>
2022-10-03 09:26:19 -0700

    9. Add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// a script that prints the addition of 2 integers
+
+function add (a, b) {
+  return (a + b);
+}
+
+console.log(add(Math.floor(Number(process.argv[2])), Math.floor(Number(process.argv[3]))));
