b2e09ac3d2db7a3b718add674c7b9d5b066e9199
Meyvel22 <meyvel22@gmail.com>
2022-10-03 09:24:52 -0700

    8. Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// a script that prints a square
+
+const mySq = Math.floor(Number(process.argv[2]));
+let rows = 'X';
+
+if (isNaN(mySq)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < mySq - 1; i++) {
+    rows += 'X';
+  }
+  for (let j = 0; j < mySq; j++) {
+    console.log(rows);
+  }
+}
