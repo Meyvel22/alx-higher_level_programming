2bdafe19faa95ea23e23058d08d9a5db86ddcf9d
Meyvel22 <meyvel22@gmail.com>
2022-10-21 13:12:08 -0700

    Done

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,13 +3,12 @@
 - fetches https://intranet.hbtn.io/status.
 - uses urlib package
 """
-
-
 import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         html = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(html)))
