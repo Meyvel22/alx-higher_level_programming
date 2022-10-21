4bf5597ccaeb53354b2566e09df0b8a4d3cc7811
Meyvel22 <meyvel22@gmail.com>
2022-10-21 09:32:56 -0700

    F Task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,10 @@
 """
 
 
-if __name__ == '__main__':
-    import urllib.request
+import urllib.request
+
+
+if __name__ == "__main__":
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         html = resp.read()
